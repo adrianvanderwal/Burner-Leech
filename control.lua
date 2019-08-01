6317995bf2af464d2fa31ea011d14dab8b053ab6
Unknown <a.vanderwaljnr@gmail.com>
2019-08-01 12:38:56 +0800

    1.0.0

--- control.lua
+++ control.lua
@@ -1,208 +1,165 @@
--- Overhaul
 function init_globals()
-  -- [re]build the list of burner/inserter entities
-  global.burner = {}
-  for _,surface in pairs(game.surfaces) do
-    for _,entity in ipairs(surface.find_entities()) do
-      if entity.force.name ~= "neutral" then
-        if string.find(entity.name, "burner") and string.find(entity.name, "inserter") then
-          local burner = entity
-          table.insert(global.burner, burner)
+    -- [re]build the list of burner/inserter entities
+    global.burners = {}
+    for _, surface in pairs(game.surfaces) do
+        for _, entity in ipairs(surface.find_entities()) do
+            if entity.force.name ~= 'neutral' then
+                if (entity.type == 'inserter' and entity.burner) then
+                    local burner = entity
+                    table.insert(global.burners, {entity = burner, position = burner.position, force = burner.force, surface = burner.surface})
+                end
+            end
         end
-      end
     end
-  end
-  global.burner_index = 1
-  -- [re]build the list of fuel items
-  global.fuel_list = {}
-  for _, proto in pairs (game.item_prototypes) do
-    if proto.fuel_value > 0 then
-      table.insert(global.fuel_list,proto.name)
+    global.burner_index = nil
+    -- [re]build the list of fuel items
+    global.fuel_list = {}
+    for _, proto in pairs(game.item_prototypes) do
+        if proto.fuel_value > 0 then
+            table.insert(global.fuel_list, proto.name)
+        end
     end
-  end
+end
+local function position_to_tile_position(position)
+    local x, y
+    local ceil_x = math.ceil(position.x)
+    local ceil_y = math.ceil(position.y)
+    x = position.x >= 0 and math.floor(position.x) + 0.5 or (ceil_x == position.x and ceil_x + 0.5 or ceil_x - 0.5)
+    y = position.y >= 0 and math.floor(position.y) + 0.5 or (ceil_y == position.y and ceil_y + 0.5 or ceil_y - 0.5)
+    return {x, y}
 end
 
-script.on_event(defines.events.on_built_entity, function(event)
-	if not string.find(event.created_entity.name, "burner") then return end
-	if not string.find(event.created_entity.name, "inserter") then return end
-  local burner = event.created_entity
-  table.insert(global.burner, burner)
-  check_burner(burner)
-end)
-
-script.on_event(defines.events.on_robot_built_entity, function(event)
-	if not string.find(event.created_entity.name, "burner") then return end
-	if not string.find(event.created_entity.name, "inserter") then return end
-  local burner = event.created_entity
-  table.insert(global.burner, burner)
-  check_burner(burner)
-end)
-
-
-script.on_event(
-    defines.events.on_tick,
-    function(event)
-        leech()
-    end
-)
-
-script.on_event(
-    defines.events.on_built_entity,
-    function(event)
-        if not string.find(event.created_entity.name, 'burner') then
-            return
-        end
-        if not string.find(event.created_entity.name, 'inserter') then
-            return
-        end
-        if not global.burner then
-            global.burner = {}
-        end
+local function on_built(event)
+    if (event.created_entity.type == 'inserter' and event.created_entity.burner) then
         local burner = event.created_entity
-        table.insert(global.burner, burner)
-        check_burner(burner)
-    end
-)
-
-script.on_event(
-    defines.events.on_robot_built_entity,
-    function(event)
-        if not string.find(event.created_entity.name, 'burner') then
-            return
+        --log(burner.name .. ' @ ' .. burner.position.x .. ', ' .. burner.position.y)
+        table.insert(global.burners, {entity = burner, position = burner.position, force = burner.force, surface = burner.surface})
+        if global.burner_index == nil then
+        --global.burner_index = next(global.burners)
         end
-        if not string.find(event.created_entity.name, 'inserter') then
-            return
-        end
-        log(event.created_entity.name .. ' @ ' .. event.created_entity.position.x .. ', ' .. event.created_entity.position.y)
-        if not global.burner then
-            global.burner = {}
-        end
-        local burner = event.created_entity
-        table.insert(global.burner, burner)
-        check_burner(burner)
+    --log(serpent.block(global.burners))
     end
-)
-
-function leech()
-    if not fuel_list then
-        --I recalculate the fuel list everytime because adding or removing mod/items can fuck it all up and this doesn't take very long
-        fuel_list = {}
-        for k, v in pairs(game.item_prototypes) do
-            if v.fuel_value > 0 then
-                table.insert(fuel_list, v)
-            end
-        end
-    end
-    if global.burner_index == nil then
-        global.burner_index = 1
-    end
-    if global.burner == nil then
-        return
-    end
-    if global.burner[global.burner_index] == nil then
-        return
-    end
-    check_burner(global.burner[global.burner_index])
-    if global.burner_index >= #global.burner then
-        global.burner_index = 1
-    else
-        global.burner_index = global.burner_index + 1
-    end
-  if #global.burner == 0 then return end
-  if check_burner(global.burner[global.burner_index]) then
-    global.burner_index = (global.burner_index % #global.burner) + 1
-  end
 end
 
-function check_burner(burner)
-  if (not burner) or (not burner.valid) then
-    table.remove(global.burner, global.burner_index)
-    return false
+script.on_event(defines.events.on_built_entity, on_built)
+script.on_event(defines.events.on_robot_built_entity, on_built)
+
+--- check_burner
+function check_burner()
+    local data
+    if global.burner_index and not global.burners[global.burner_index] then
+        game.print('Invalid burner_index ' .. global.burner_index)
+        global.burner_index = nil
     end
-    local surface = burner.surface
-    local burners = surface.find_entities_filtered({position = burner.position, force = burner.force, surface = burner.surface})
-    if not burners == nil then
-        burner = burners[1]
-    end
-    if not burner.valid then
-        table.remove(global.burner, global.burner_index)
-        global.burner_index = global.burner_index - 1
-        log('return')
-        return
-    end
-
-    local send_to_target = false
-    local take_from_inventory = false
-    local pickup_target = {}
-    local pt, dt
-
-    if burner.pickup_target == nil then
-        pt = surface.find_entities_filtered({position = burner.pickup_position, force = burner.force, surface = burner.surface, limit = 1})
-        if pt[1] ~= nil then
-            log('pickup_target = ' .. pt[1].name .. ' @ ' .. pt[1].position.x .. ', ' .. pt[1].position.y)
-            if pt[1].get_fuel_inventory() ~= nil then
-                take_from_inventory = true
-                pickup_target = pt[1]
-            end
-        end
-    else
-        pickup_target = burner.pickup_target
-    end
-
-    -- check self fuel
-    -- if > 1 fuel, check drop_position (?)
-
-    if burner.get_fuel_inventory().get_item_count() < 1 then
-        burner.drop_target = burner
-    else
-        dt = surface.find_entities_filtered({position = burner.drop_position, force = burner.force, surface = burner.surface, limit = 1})
-        burner.drop_target = dt[1]
-    end
-
-    --log(serpent.block(burner.drop_position))
-
-    if burner.drop_target ~= nil then
-        if not string.find(burner.drop_target.name, 'burner') then
-            return
-        end
-        log('drop_target = ' .. burner.drop_target.name .. ' @ ' .. burner.drop_target.position.x .. ', ' .. burner.drop_target.position.y)
-        if burner.drop_target.get_fuel_inventory() ~= nil then
-            if burner.drop_target.get_fuel_inventory().get_item_count() < 1 then
-                send_to_target = true
-            end
-        end
-    end
-
-    log(serpent.block(pickup_target))
-
-    if next(pickup_target) == nil then
-        return
-    end
-
-    log(send_to_target)
-
-    if burner.get_item_count() < 1 or send_to_target then
-        leeched = pickup_target
-        log(serpent.block(leeched))
-        if leeched == nil then
-            log('return')
-            return
-        end
-        if burner.held_stack.valid_for_read == false then
-            log('valid_for_read = false')
-            for j, v in pairs(fuel_list) do
-                log(v.name .. ' ' .. leeched.get_item_count(v.name))
-                if leeched.get_item_count(v.name) > 0 then
-                    burner.held_stack.set_stack({name = v.name, count = 1})
-                    leeched.remove_item({name = v.name, count = 1})
+    global.burner_index, data = next(global.burners, global.burner_index)
+    if data ~= nil then
+        local burner = data.entity
+        if burner then
+            if burner.valid then
+                --log(burner.name .. ' @ ' .. burner.position.x .. ', ' .. burner.position.y)
+                leech(burner)
+            else
+                -- burner has been removed
+                -- check to see if there is a different burner inserter at that position - use that, else remove the reference
+                local position = data.position
+                local surface = data.surface
+                bc = surface.find_entities_filtered({position = data.position, force = data.force, surface = surface, limit = 1})
+                if next(bc) == nil then
+                    -- NOTHING WAS FOUND
+                    --log('burner invalid, removing')
+                    global.burners[global.burner_index] = nil
+                    global.burner_index = nil
                     return
+                else
+                    -- replace the reference
+                    global.burners[global.burner_index].entity = bc[1]
+                    leech(bc[1])
                 end
             end
         end
     end
 end
 
-script.on_event(defines.events.on_tick, leech)
+--- leech(burner)
+-- checks to see if the burner inserter can/should leech fuel from the entity at it's pickup position
+function leech(burner)
+    --log('Testing leech options for ' .. burner.name .. ' @ ' .. burner.position.x .. ', ' .. burner.position.y)
+    local surface, force, position = burner.surface, burner.force, burner.position
+
+    local pickup_target, take_from_pickup_target_inventory = nil, false
+    local drop_target, send_to_target = nil, false
+
+    -- find and set pickup_target
+    if burner.pickup_target == nil then
+        --log('finding possible pickup_target')
+        --log(serpent.block(position_to_tile_position(burner.pickup_position)))
+        pt = surface.find_entities_filtered({position = position_to_tile_position(burner.pickup_position), force = burner.force, surface = burner.surface, limit = 1})
+        if pt[1] ~= nil then
+            --log('pickup_target = ' .. pt[1].name .. ' @ ' .. pt[1].position.x .. ', ' .. pt[1].position.y)
+            if pt[1].get_fuel_inventory() ~= nil then
+                take_from_pickup_target_inventory = true
+                pickup_target = pt[1]
+            end
+        end
+    else
+        pickup_target = burner.pickup_target
+    end
+    -- nothing to pickup from
+    if pickup_target == nil then
+        --log('No pickup_target.')
+        return
+    end
+
+    --log('pickup_target = ' .. pickup_target.name .. ' @ ' .. pickup_target.position.x .. ', ' .. pickup_target.position.y)
+
+    -- find and set drop_target
+    -- if self fuel count < 1 fuel self before fueling others
+    if burner.get_fuel_inventory().get_item_count() < 1 then
+        drop_target = burner
+    else
+        --log('finding possible drop_target')
+        --log(serpent.block(position_to_tile_position(burner.drop_position)))
+        dt = surface.find_entities_filtered({position = position_to_tile_position(burner.drop_position), force = burner.force, surface = burner.surface, limit = 1})
+        drop_target = dt[1]
+    end
+
+    -- nothing to drop to
+    if drop_target == nil then
+        --log('No drop_target.')
+        return
+    end
+
+    --log('drop_target = ' .. drop_target.name .. ' @ ' .. drop_target.position.x .. ', ' .. drop_target.position.y)
+
+    -- check drop_target for burner energy source
+    if drop_target.burner == nil then
+        --log('drop_target has no burner energy source')
+        return
+    end
+
+    if drop_target.get_fuel_inventory() ~= nil then
+        if drop_target.get_fuel_inventory().get_item_count() < 1 then
+            send_to_target = true
+        else
+            return
+        end
+    end
+
+    --log(drop_target.name .. ' @ ' .. drop_target.position.x .. ', ' .. drop_target.position.y .. ' requires refuelling')
+
+    if burner.held_stack.valid_for_read == false then
+        log('valid_for_read = false')
+        for _, fuel in pairs(global.fuel_list) do
+            if pickup_target.get_item_count(fuel) > 0 then
+                burner.held_stack.set_stack({name = fuel, count = 1})
+                pickup_target.remove_item({name = fuel, count = 1})
+                return true
+            end
+        end
+    end
+end
+
+script.on_nth_tick(1, check_burner)
 
 script.on_init(init_globals)
 
